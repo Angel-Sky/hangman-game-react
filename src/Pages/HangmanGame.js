0fcaae364b8016129fcfbaef8a78c1ae7b668b6d
Iva Tosheva <otevi55@gmail.com>
2022-05-27 17:03:25 +0300

    Visualize random letters

--- src/Pages/HangmanGame.js
+++ src/Pages/HangmanGame.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import {randomWord} from '../components/RandomWord'
+import { randomWord, lettersToVisualize } from '../services/Words'
 import step0 from "../images/step0.png";
 import step1 from "../images/step1.png";
 import step2 from "../images/step2.png";
@@ -18,12 +18,15 @@ function HangmanGame() {
     const [maxWrong] = useState(10);
     const [images] = useState([step0, step1, step2, step3, step4, step5, step6, step7, step8, step8, step9, step10]);
     const [guessed, setguessed] = useState(new Set([]));
-    const [word] = useState(randomWord)
+    const [word] = useState(randomWord);
+    const [letters] = useState(lettersToVisualize(word));
+
     return (
-        <>  
+        <>
             <h1>Hangman Game</h1>
             <img src={images[mistakes]}></img>
             <p>{word}</p>
+            <div>{letters.map(x => <button key={x}>{x}</button>)}</div>
         </>
     )
 }
