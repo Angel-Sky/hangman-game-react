c2368a875dd213f4707385bd953e65bc9a5d7a2e
Iva Tosheva <otevi55@gmail.com>
2022-05-30 14:46:07 +0300

    Add progress bar for player lives

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,13 +1,29 @@
 import './Score.css'
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import GameContext from "../context/GameContext";
+import { Progress } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Score() {
-    const [{mistakes, maxMistakes}] = useContext(GameContext);
+    const [{ mistakes, maxMistakes }] = useContext(GameContext);
+    const [progressColor, setProgressColor] = useState('success')
+
+    useEffect(() => {
+        if (mistakes <= 5) {
+            setProgressColor('success')
+        } else if (mistakes > 5 && mistakes <= 7) {
+            setProgressColor('warning')
+        } else {
+            setProgressColor('danger')
+        }
+    }, [mistakes])
 
     return (
-        <div id="score">
-            Score <b>{mistakes}</b> out of <b>{maxMistakes}</b>
+        <div className='row justify-content-md-center'>
+            <div id="score" className='col-6 text-center'>
+                <div> Score <b>{mistakes}</b> out of <b>{maxMistakes}</b></div>
+                <Progress striped value={100 - mistakes * 10} color={progressColor} />
+            </div>
         </div>
     )
 }
